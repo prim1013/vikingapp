9580ffedc935bd4a586c8527396c3e74b36ea8f9
unknown <PrimaveC@0108M308757L.corp.espn.pvt>
2014-06-20 06:47:24 -0400

    configuring ratchet stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,12 +20,38 @@ module.exports = function ( grunt ) {
                     ]
                 }
             }
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ['public/css/lib/ratchet']
+                },
+                files: {
+                    "public/css/lib/ratchet/docs.less": "public/css/lib/ratchet/docs.less",
+                    "public/css/lib/ratchet/ratchet.less": "public/css/lib/ratchet/ratchet.less",
+                    "public/css/lib/ratchet/ratchet-theme-ios.less": "public/css/lib/ratchet/ratchet-theme-ios.less"
+                }
+            },
+            production: {
+                options: {
+                    paths: ["assets/css"],
+                    cleancss: true,
+                    modifyVars: {
+                        imgPath: '"http://mycdn.com/path/to/images"',
+                        bgColor: 'red'
+                    }
+                },
+                files: {
+                    "path/to/result.css": "path/to/source.less"
+                }
+            }
         }
     } );
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    grunt.loadNpmTasks( 'grunt-contrib-less' );
 
     // Default task(s).
-    grunt.registerTask( 'default', ['uglify'] );
+    grunt.registerTask( 'default', ['uglify', 'less'] );
 };
