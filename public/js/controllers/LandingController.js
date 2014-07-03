b721b6f482feb712c64d301545015c3ee3f8b01e
unknown <PrimaveC@0108M308757L.corp.espn.pvt>
2014-07-03 09:23:04 -0400

    add modernizr check for touch

--- public/js/controllers/LandingController.js
+++ public/js/controllers/LandingController.js
@@ -1,19 +1,10 @@
 'use strict';
 
 viking.controller( 'LandingController', function LandingController ( $scope ) {
+    var touch = Modernizr.touch;
 
     $( '.img-holder' ).imageScroll( {
-//            image: null,
-//            imageAttribute: 'image',
-//            container: $('body'),
-//            speed: 0.2,
-//            coverRatio: 0.75,
-//            holderClass: 'imageHolder',
-//            holderMinHeight: 200,
-//            extraHeight: 0,
-//            mediaWidth: 1600,
-//            mediaHeight: 900,
-//            parallax: true,
-//            touch: false
+        imageAttribute: (touch === true) ? 'image-mobile' : 'image',
+        touch: touch
     } );
 } );
