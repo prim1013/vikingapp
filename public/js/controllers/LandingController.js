a09271860051cf6f3cca4642a3ac1c3c78b837ab
unknown <primavec@0108M308757L.corp.espn.pvt>
2014-07-28 14:37:49 -0400

    integrate skrollr and set up initial flow - much more to come

--- public/js/controllers/LandingController.js
+++ public/js/controllers/LandingController.js
@@ -1,10 +1,12 @@
 'use strict';
 
 viking.controller( 'LandingController', function LandingController ( $scope ) {
-    var touch = Modernizr.touch;
+//    var touch = Modernizr.touch;
+//
+//    $( '.img-holder' ).imageScroll( {
+//        imageAttribute: (touch === true) ? 'image-mobile' : 'image',
+//        touch: touch
+//    } );
 
-    $( '.img-holder' ).imageScroll( {
-        imageAttribute: (touch === true) ? 'image-mobile' : 'image',
-        touch: touch
-    } );
+    var s = skrollr.init();
 } );
