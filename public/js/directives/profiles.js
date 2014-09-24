c491e49f311190f4ccefd564e8a7398c8aa9bb25
unknown <primavec@0108M308757L.corp.espn.pvt>
2014-09-24 08:30:37 -0400

    refactor profiles into directive

---
+++ public/js/directives/profiles.js
@@ -0,0 +1,9 @@
+'use strict';
+
+viking.directive( 'profiles', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'templates/directives/profiles.html'
+    }
+} );
